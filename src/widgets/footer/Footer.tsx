768703e350d6feb8863c4dae4a0a083bbdd4ed02
Timothy7310 <penlist127@gmail.com>
2023-04-28 16:44:21 +0300

    feat: add footer

--- src/widgets/footer/Footer.tsx
+++ src/widgets/footer/Footer.tsx
@@ -1,3 +1,21 @@
 import styles from './Footer.module.scss';
 
-export const Footer = () => <footer className={styles.footer}>RSS 2023</footer>;
+export const Footer = () => (
+    <footer className={styles.footer}>
+        <div className={styles.bounding}>
+            <div className={styles.inner}>
+                <div className={styles.authors}>
+                    <a className={styles.links} href="#" target='_blank'>Author I</a>
+                    <a className={styles.links} href="#" target='_blank'>Author II</a>
+                    <a className={styles.links} href="#" target='_blank'>Author III</a>
+                </div>
+                <span className={styles.year}>2023</span>
+                <a className={styles.logo} href="https://rs.school/js/">
+                    <svg className={styles.icon}>
+                        <use href="src/assets/img/sprite.svg#rss-logo"></use>
+                    </svg>
+                </a>
+            </div>
+        </div>
+    </footer>
+);

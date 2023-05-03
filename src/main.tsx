90ba1f0900d2abba6136d9ba8f35384f9ca0ed7e
bloodsuckers-spb <bloodsuckers-spb@hotmail.com>
2023-05-03 18:30:26 +0300

    feat: remove firebase import

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
-import '../firebase';
-
 import { App } from './app';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,26 +1,12 @@
 /* eslint-disable import/no-default-export */
 
-import { loginSlice } from 'app/providers/StoreProvider/config/reducers';
 import { UserAuth } from 'features/user-auth';
-import { Link } from 'react-router-dom';
-import { useAppDispatch } from 'shared/hooks/redux';
 
 import styles from './Login.module.scss';
 
 const Login = () => {
-  const dispatch = useAppDispatch();
-  const handleSubmit = () => {
-    dispatch(loginSlice.actions.signIn());
-    localStorage.setItem('isLogin', 'true');
-  };
   return (
     <div className={styles.content}>
-      <Link
-        to="/editor"
-        onClick={handleSubmit}
-      >
-        Submit
-      </Link>
       <UserAuth />
     </div>
   );

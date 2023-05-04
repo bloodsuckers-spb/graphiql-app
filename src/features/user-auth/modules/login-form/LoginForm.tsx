22d19d90f53bf3cf8eec2f73f46a4fdd7361597a
bloodsuckers-spb <bloodsuckers-spb@hotmail.com>
2023-05-05 01:52:01 +0300

    feat: update LoginForm component

--- src/features/user-auth/modules/login-form/LoginForm.tsx
+++ src/features/user-auth/modules/login-form/LoginForm.tsx
@@ -1,9 +1,13 @@
 import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
 
+import { FormEvent, useRef } from 'react';
+
 import { AuthForm, FormBtn, UserAuthField, FormInput } from 'shared/ui';
 
 export const LoginForm = () => {
-  const handleLogin = async (email: string, password: string) => {
+  const handleLogin = async () => {
+    const email = '';
+    const password = 'string';
     try {
       const auth = getAuth();
       const userCredential = await signInWithEmailAndPassword(
@@ -16,15 +20,20 @@ export const LoginForm = () => {
     }
   };
 
+  const onSubmit = (event: FormEvent) => {
+    event.preventDefault();
+    handleLogin();
+  };
+
   return (
-    <AuthForm>
+    <AuthForm onSubmit={onSubmit}>
       <UserAuthField>
         <FormInput type="email" />
       </UserAuthField>
       <UserAuthField>
         <FormInput type="password" />
       </UserAuthField>
-      <FormBtn handleClick={handleLogin} />
+      <FormBtn />
     </AuthForm>
   );
 };

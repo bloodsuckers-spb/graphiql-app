4c33cf28c67c6218b0d7105badc74b9a8e62ada0
timothy <penlist127@gmail.com>
2023-05-05 20:03:38 +0300

    feat: add sticky header

--- src/widgets/header/Header.tsx
+++ src/widgets/header/Header.tsx
@@ -1,6 +1,9 @@
 import { loginSlice } from 'app/providers/StoreProvider/config/reducers';
+import { useEffect, useRef, useState } from 'react';
 import { Link, NavLink } from 'react-router-dom';
 import { useAppSelector, useAppDispatch } from 'shared/hooks/redux';
+
+import { classNames } from 'shared/libs';
 
 import { Wrapper, AppLogo } from 'shared/ui';
 
@@ -9,6 +12,10 @@ import styles from './Header.module.scss';
 export const Header = () => {
   const dispatch = useAppDispatch();
   const isLogin = useAppSelector((state) => state.loginReducer.value);
+
+  const [isSticky, setSticky] = useState(false);
+
+  const headerRef = useRef(null);
 
   const signOut = () => {
     dispatch(loginSlice.actions.signOut());
@@ -21,8 +28,39 @@ export const Header = () => {
       : `${styles.nav__link}`;
   };
 
+  const handleScroll = (elTopOffset: number, elHeight: number) => {
+    if (window.pageYOffset > elTopOffset + elHeight) {
+      setSticky(true);
+    } else {
+      setSticky(false);
+    }
+  };
+
+  useEffect(() => {
+    const header = (
+      headerRef?.current as unknown as HTMLElement
+    ).getBoundingClientRect();
+
+    window.addEventListener('scroll', () =>
+      handleScroll(header.top, header.height)
+    );
+
+    return () => {
+      window.removeEventListener('scroll', () =>
+        handleScroll(header.top, header.height)
+      );
+    };
+  }, []);
+
   return (
-    <header className={styles.header}>
+    <header
+      className={
+        isSticky
+          ? classNames(styles.header, {}, [styles.headerActive])
+          : styles.header
+      }
+      ref={headerRef}
+    >
       <Wrapper className={styles.inner__header}>
         <div className={styles.logo}>
           <AppLogo />

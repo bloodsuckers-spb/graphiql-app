65c57fdc726fb64d59cf0d95970ebc36a43f7e17
timothy <penlist127@gmail.com>
2023-05-05 19:13:53 +0300

    refactor: map authors in Footer

--- src/widgets/footer/Footer.tsx
+++ src/widgets/footer/Footer.tsx
@@ -1,41 +1,28 @@
 import { Wrapper } from 'shared/ui';
 
+import { authors } from './authors';
+
 import styles from './Footer.module.scss';
 
 export const Footer = () => (
   <footer className={styles.footer}>
     <Wrapper className={styles.inner__footer}>
       <div className={styles.authors}>
-        <a
-          className={styles.links}
-          href="https://github.com/bloodsuckers-spb"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <svg className={styles.authors__icon}>
-            <use href="./sprite.svg#github"></use>
-          </svg>
-        </a>
-        <a
-          className={styles.links}
-          href="https://github.com/timothy7310"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <svg className={styles.authors__icon}>
-            <use href="./sprite.svg#github"></use>
-          </svg>
-        </a>
-        <a
-          className={styles.links}
-          href="https://github.com/criphood"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <svg className={styles.authors__icon}>
-            <use href="./sprite.svg#github"></use>
-          </svg>
-        </a>
+        {authors.map((author) => {
+          return (
+            <a
+              className={styles.links}
+              href={author.link}
+              target="_blank"
+              rel="noopener noreferrer"
+              key={author.id}
+            >
+              <svg className={styles.authors__icon}>
+                <use href="./sprite.svg#github"></use>
+              </svg>
+            </a>
+          );
+        })}
       </div>
       <span className={styles.year}>2023</span>
       <a

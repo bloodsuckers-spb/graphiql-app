ecf99a2381cb3d81e6b7a406ed0fde12d529a6d5
Timothy7310 <penlist127@gmail.com>
2023-05-10 18:28:28 +0300

    feat: add Editor page

--- src/pages/editor/Editor.tsx
+++ src/pages/editor/Editor.tsx
@@ -2,6 +2,13 @@
 import { auth } from 'app/firebase';
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { Wrapper } from 'shared/ui';
+import EditorCode from 'shared/ui/editor/EditorCode';
+import { extensions } from 'shared/ui/editor/settings/extensions';
+import { reqTheme, resTheme } from 'shared/ui/editor/settings/themes';
+import EditorControls from 'shared/ui/editorControls/EditorControls';
+
+import styles from './Editor.module.scss';
 
 const Editor = () => {
   const user = auth.currentUser;
@@ -13,7 +20,27 @@ const Editor = () => {
     }
   }, [user, navigate]);
 
-  return <div>Editor page</div>;
+  return (
+    <Wrapper className={styles.innerEditor}>
+      <div className={styles.wrapper}>
+        <div className={styles.content}>
+          <div className={styles.playGround}>
+            <EditorCode
+              theme={reqTheme}
+              extensions={extensions}
+              type="request"
+            />
+            <EditorControls />
+          </div>
+          <EditorCode
+            theme={resTheme}
+            extensions={[]}
+            type="response"
+          />
+        </div>
+      </div>
+    </Wrapper>
+  );
 };
 
 export default Editor;

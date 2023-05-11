882c22dfc4a798eea4c5fe7b7bf908079fde8f57
Admin <criphood4life@gmail.com>
2023-05-12 01:01:30 +0300

    fix: top level await

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import topLevelAwait from 'vite-plugin-top-level-await';
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    topLevelAwait({
+      promiseExportName: '__tla',
+      promiseImportName: (i) => `__tla_${i}`,
+    }),
+  ],
   css: {
     preprocessorOptions: {
       scss: {

49fe81bd7b582a3f2e0d469143d59eb6dff29159
bloodsuckers-spb <bloodsuckers-spb@hotmail.com>
2023-05-17 17:06:40 +0300

    feat: replace useEffect with redirect to Login page

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,10 +1,21 @@
 /* eslint-disable import/no-default-export */
-
+import { auth } from 'app/firebase';
 import { UserAuth } from 'features/user-auth';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import styles from './Login.module.scss';
 
 const Login = () => {
+  const user = auth.currentUser;
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (user) {
+      navigate('/editor');
+    }
+  }, [user, navigate]);
+
   return (
     <div className={styles.content}>
       <UserAuth />

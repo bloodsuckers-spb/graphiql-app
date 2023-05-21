6a60cfce5762c1e1159e1b81fd2660f89597612f
bloodsuckers-spb <bloodsuckers-spb@hotmail.com>
2023-05-21 23:09:04 +0300

    feat: replace react-firebase-hook from depDeps to Deps

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { auth } from 'app/firebase';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { ToastContainer, toast } from 'react-toastify';

99dcf625fe194342b7751812e0c90c20a6f45c11
Timothy7310 <penlist127@gmail.com>
2023-05-26 15:09:30 +0300

    refactor: hide unused code

--- src/features/user-auth-form/UserAuthForm.tsx
+++ src/features/user-auth-form/UserAuthForm.tsx
@@ -1,74 +1,74 @@
-import { auth } from 'app/firebase';
-import {
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword,
-} from 'firebase/auth';
+// import { auth } from 'app/firebase';
+// import {
+//   createUserWithEmailAndPassword,
+//   signInWithEmailAndPassword,
+// } from 'firebase/auth';
 
-import { useForm, SubmitHandler } from 'react-hook-form';
+// import { useForm, SubmitHandler } from 'react-hook-form';
 
-import { useTranslation } from 'react-i18next';
+// import { useTranslation } from 'react-i18next';
 
-import { UserAuthField, FormBtn } from 'shared/ui';
+// import { UserAuthField, FormBtn } from 'shared/ui';
 
-import { Props, FormFields } from './types';
+// import { Props, FormFields } from './types';
 
-import styles from './UserAuthForm.module.scss';
+// import styles from './UserAuthForm.module.scss';
 
-const signIn = async ({ email, password }: FormFields) => {
-  try {
-    await signInWithEmailAndPassword(auth, email, password);
-  } catch (error: unknown) {
-    if (error instanceof Error) {
-      console.log(error.message);
-    }
-  }
-};
+// const signIn = async ({ email, password }: FormFields) => {
+//   try {
+//     await signInWithEmailAndPassword(auth, email, password);
+//   } catch (error: unknown) {
+//     if (error instanceof Error) {
+//       console.log(error.message);
+//     }
+//   }
+// };
 
-const signUp = async ({ email, password }: FormFields) => {
-  try {
-    await createUserWithEmailAndPassword(auth, email, password);
-  } catch (error: unknown) {
-    if (error instanceof Error) {
-      console.log(error.message);
-    }
-  }
-};
+// const signUp = async ({ email, password }: FormFields) => {
+//   try {
+//     await createUserWithEmailAndPassword(auth, email, password);
+//   } catch (error: unknown) {
+//     if (error instanceof Error) {
+//       console.log(error.message);
+//     }
+//   }
+// };
 
-export const UserAuthForm = ({ isSignUp }: Props) => {
-  const {
-    register,
-    handleSubmit,
-    // formState: { errors },
-  } = useForm<FormFields>();
+// export const UserAuthForm = ({ isSignUp }: Props) => {
+//   const {
+//     register,
+//     handleSubmit,
+//     // formState: { errors },
+//   } = useForm<FormFields>();
 
-  const onSubmit: SubmitHandler<FormFields> = (data) => {
-    isSignUp ? signUp(data) : signIn(data);
-  };
+//   const onSubmit: SubmitHandler<FormFields> = (data) => {
+//     isSignUp ? signUp(data) : signIn(data);
+//   };
 
-  const { t } = useTranslation();
+//   const { t } = useTranslation();
 
-  return (
-    <form
-      className={styles.form}
-      onSubmit={handleSubmit(onSubmit)}
-    >
-      <UserAuthField>
-        <input
-          className={styles.input}
-          type="email"
-          placeholder={`${t('formEmail')}`}
-          {...register('email')}
-        />
-      </UserAuthField>
-      <UserAuthField>
-        <input
-          className={styles.input}
-          type="password"
-          placeholder={`${t('formPassword')}`}
-          {...register('password')}
-        />
-      </UserAuthField>
-      <FormBtn />
-    </form>
-  );
-};
+//   return (
+//     <form
+//       className={styles.form}
+//       onSubmit={handleSubmit(onSubmit)}
+//     >
+//       <UserAuthField>
+//         <input
+//           className={styles.input}
+//           type="email"
+//           placeholder={`${t('formEmail')}`}
+//           {...register('email')}
+//         />
+//       </UserAuthField>
+//       <UserAuthField>
+//         <input
+//           className={styles.input}
+//           type="password"
+//           placeholder={`${t('formPassword')}`}
+//           {...register('password')}
+//         />
+//       </UserAuthField>
+//       <FormBtn />
+//     </form>
+//   );
+// };

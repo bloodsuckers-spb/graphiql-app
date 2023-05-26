1d0d5da585e3296556b3ad2c48ea1c186b9dc061
bloodsuckers-spb <bloodsuckers-spb@hotmail.com>
2023-05-26 12:52:36 +0300

    feat: update reg exp for validation

--- src/widgets/graph-ql-editor/modules/index.ts
+++ src/widgets/graph-ql-editor/modules/index.ts
@@ -3,3 +3,5 @@ export * from './request-editor';
 export * from './response-output';
 export * from './options-editor';
 export * from './editor-api-docs';
+export * from './editor-docs-root';
+export * from './editor-docs-non-root';

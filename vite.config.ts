78e5d60a141e292dd5fb1d1d28db798229caf561
bloodsuckers-spb <bloodsuckers-spb@hotmail.com>
2023-05-28 17:02:57 +0300

    refactor: change sass imports to forward and use

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,20 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
   css: {
     preprocessorOptions: {
       scss: {
-        additionalData: `@import "src/app/styles/index.scss";`,
+        additionalData: `@use "src/app/styles/_index.scss" as *;`,
       },
+    },
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
     },
   },
 });

b6a04f65272729a8bc54c08c4c825232626eb7dc
bloodsuckers-spb <bloodsuckers-spb@hotmail.com>
2023-05-28 18:46:20 +0300

    refactor: replace schemaApi

--- src/app/providers/StoreProvider/config/reducers/editorSlice/default.ts
+++ src/app/providers/StoreProvider/config/reducers/editorSlice/default.ts
@@ -1,6 +1,6 @@
 import { GraphQLSchema } from 'graphql';
 
 export const DefaultSchema = new GraphQLSchema({});
-// export const DefaultURL = 'https://rickandmortyapi.com/graphql';
+export const DefaultURL = 'https://rickandmortyapi.com/graphql';
 
-export const DefaultURL = 'https://spacex-production.up.railway.app';
+// export const DefaultURL = 'https://spacex-production.up.railway.app';
--- src/app/providers/StoreProvider/config/reducers/index.ts
+++ src/app/providers/StoreProvider/config/reducers/index.ts
@@ -1,2 +1,2 @@
 export * from './editorSlice';
-export * from './schemaApi';
+export * from '../services/schemaApi';
